4a8bae1ca37dc52d7ced76d99626edb466bc394c
m.ugryumov <m.ugryumov@vkteam.ru>
2025-04-05 12:08:17 +0300

    подключил минио

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -11,6 +11,8 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 	"purify/src/cache"
 	"purify/src/chatgpt"
 	"purify/src/config"
@@ -29,7 +31,30 @@ func init() {
 	}
 }
 
+func createMinioBucketIfNotExists(minioClient *minio.Client, bucketName string, region string, logger *slog.Logger) error {
+	ctx := context.Background()
+
+	exists, err := minioClient.BucketExists(ctx, bucketName)
+	if err != nil {
+		return errors.Wrap(err, "failed to check bucket existence")
+	}
+
+	if !exists {
+		if err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: region}); err != nil {
+			return errors.Wrap(err, "failed to create bucket")
+		}
+		logger.Info("minio bucket created")
+	} else {
+		logger.Info("minio bucket already exists")
+	}
+
+	return nil
+}
+
 func main() {
+	// =================================================================================================================
+	// logger
+
 	logFile, err := os.OpenFile(os.Getenv("MAIN_LOG_FILE"), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		log.Fatalf("failed to open log file: %v", err)
@@ -38,8 +63,16 @@ func main() {
 
 	logger := slog.New(slog.NewJSONHandler(io.MultiWriter(logFile, os.Stdout), &slog.HandlerOptions{Level: slog.LevelInfo}))
 
+	// logger
+	// =================================================================================================================
+	// config
+
 	cfg := config.MustLoadConfig(os.Getenv("CONFIG_FILE"), logger)
 	logger.Info("Config file loaded")
+
+	// config
+	// =================================================================================================================
+	// redis
 
 	redisOpts, err := redis.ParseURL(os.Getenv("REDIS_URL"))
 	if err != nil {
@@ -54,10 +87,42 @@ func main() {
 	}
 	logger.Info("Redis connected")
 
+	// redis
+	// =================================================================================================================
+	// minio
+
+	endpoint := os.Getenv("MINIO_ENDPOINT")
+	accessKeyID := os.Getenv("MINIO_ROOT_USER")
+	secretAccessKey := os.Getenv("MINIO_ROOT_PASSWORD")
+	region := os.Getenv("MINIO_REGION")
+
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: false,
+		Region: region,
+	})
+	if err != nil {
+		logger.Error(errors.Wrap(err, "failed to initialize minio client").Error())
+		return
+	}
+	logger.Info("Minio connected")
+
+	if err = createMinioBucketIfNotExists(minioClient, cfg.Minio.BucketName, region, logger); err != nil {
+		logger.Error(errors.Wrap(err, "minio init error").Error())
+	}
+
+	// minio
+	// =================================================================================================================
+	// entities
+
 	redisCache := cache.NewCache(redisClient)
 
 	mistralAI := mistral_ai.NewMistralAI(cfg.MistralAI)
 	chatGPT := chatgpt.NewChatGPT(cfg.ChatGPT, redisCache)
+
+	// entities
+	// =================================================================================================================
+	// router
 
 	reqIDMiddleware := middleware.CreateRequestIDMiddleware(logger)
 
@@ -84,6 +149,10 @@ func main() {
 		IdleTimeout:       cfg.Main.IdleTimeout,
 	}
 
+	// router
+	// =================================================================================================================
+	// graceful shutdown
+
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
 
@@ -103,4 +172,7 @@ func main() {
 	if err = server.Shutdown(ctx); err != nil {
 		logger.Error(errors.Wrap(err, "failed to gracefully shutdown").Error())
 	}
+
+	// graceful shutdown
+	// =================================================================================================================
 }
